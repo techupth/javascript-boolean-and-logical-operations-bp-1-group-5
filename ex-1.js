e59da0229fdef40c679ad089465749b5199f204d
“Phurikanya <phurikanya.ch@gmai.com>
2023-09-17 20:33:22 +0700

    Ex-1 is Donr

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,10 @@
 // Exercise #1: Event Conditions
 
 // Start coding here
-let isOver18 = 19;
-let hasCriminalBlacklist;
-let isThai;
+let isOver18 = true;
+let hasCriminalBlacklist = false;
+let isThai = false;
 
-let isAllow;
+let isAllow = isOver18 && !hasCriminalBlacklist && !isThai;
 
 console.log(isAllow);
