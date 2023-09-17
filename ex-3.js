0e21855306b7d32230e14783f71de135cc134825
“Phurikanya <phurikanya.ch@gmai.com>
2023-09-17 20:54:54 +0700

    All Excersice Done

--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,16 @@
 //Exercise #3: Debugging (Type Conversion)
 
 //Start coding here
-const numberA = 100;
-const numberB = "20";
+let numberA = 100;
+let numberB = "20";
+
+console.log(typeof numberA)
+console.log(typeof numberB)
 
 numberA = 10;
+console.log(numberA)
+
+numberB = Number(numberB)
+console.log(typeof numberB)
 
 console.log(`Sum: ${numberA + numberB}`);
