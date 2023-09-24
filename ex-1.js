13a2df35d1cc001ad9d6ea3077f94c058d658a67
sirithanatharathorntv <sirithanatharathorntv@gmail.com>
2023-09-24 18:08:17 +0700

    branch pop

--- ex-1.js
+++ ex-1.js
@@ -1,10 +1,11 @@
 // Exercise #1: Event Conditions
 
 // Start coding here
-let isOver18 = 19;
-let hasCriminalBlacklist;
-let isThai;
+let isOver18 = false;
+let hasCriminalBlacklist = false;
+let isThai = false;
 
-let isAllow;
+let isAllow = isOver18 || (hasCriminalBlacklist && isThai);
 
 console.log(isAllow);
+
--- ex-3.js
+++ ex-3.js
@@ -1,9 +1,9 @@
 //Exercise #3: Debugging (Type Conversion)
 
 //Start coding here
-const numberA = 100;
-const numberB = "20";
+let numberA = 100;
+let numberB = "20";
 
 numberA = 10;
-
+numberB = Number(numberB);
 console.log(`Sum: ${numberA + numberB}`);
